3fda59716594bd21621ea09d3ed63cb1ac640ab8
Buggg43 <kacperwerra12@gmail.com>
2025-07-19 22:38:33 +0200

    Adding Tests

---
+++ BookLibraryApi.Tests/Common/TestFactory.cs
@@ -0,0 +1,40 @@
+﻿using BookLibraryApi.Data;
+using BookLibraryApi.Models;
+using BookLibraryApi.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace BookLibraryApi.Tests.Common
+{
+    public static class TestFactory
+    {
+        public static LibraryDbContext CreateContext(string dbName = "TestDb")
+        {
+            var options = new DbContextOptionsBuilder<LibraryDbContext>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+            
+            return new LibraryDbContext(options);
+        }
+
+        public static User CreateTestUser(string username, string password, out PasswordHasher<User> hasher)
+        {
+            var user = new User { Username = username, Role = "User" };
+            hasher = new PasswordHasher<User>();
+            user.PasswordHash = hasher.HashPassword(user, password);
+            return user;
+        }
+
+        public static Mock<JwtService> CreateJwtServiceMock()
+        {
+            var mock = new Mock<JwtService>();
+            mock.Setup(x => x.GenerateToken(It.IsAny<User>()))
+                .Returns("access_test");
+            mock.Setup(x => x.GenerateRefreshToken(It.IsAny<User>()))
+                .Returns(new RefreshToken { Token = "refresh_token" });
+
+            return mock;
+        }
+    }
+}
---
+++ BookLibraryApi.Tests/Features/Users/Commands/LoginUserCommandHandlerTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BookLibraryApi.Data;
+using BookLibraryApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using BookLibraryApi.Services;
+using BookLibraryApi.Features.Users.Commands;
+using BookLibraryApi.Models.Dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
+using BookLibraryApi.Tests.Common;
+
+namespace BookLibraryApi.Tests.Features.Users.Commands
+{
+    
+    public class LoginUserCommandHandlerTest 
+    {
+
+        [Fact]
+        public async Task Handle_ShouldReturnTokenPair_WhenCredentialsAreValid()
+        {
+            var context = TestFactory.CreateContext();
+            var user = TestFactory.CreateTestUser("123", "123", out var hasher);
+            var mockJwtService = TestFactory.CreateJwtServiceMock();
+            context.Users.Add(user); context.SaveChanges();
+
+
+
+            var handler = new LoginUserCommandHandler(context, hasher, mockJwtService.Object);
+
+
+            var inputForTest= new List<LoginUserDto>();
+            inputForTest.Add(new LoginUserDto { Password = "123", Username = "123" });
+
+            inputForTest.Add(new LoginUserDto { Password = "1234", Username = "123" });
+
+            foreach (var input in inputForTest) {
+                var command = new LoginUserCommand(input);
+                var result = await handler.Handle(command, CancellationToken.None);
+
+                var okResult = result as Ok<TokenPairDto>;
+
+                Assert.NotNull(okResult);
+                Assert.Equal("access_test", okResult.Value.AccessToken);
+                Assert.Equal("refresh_test", okResult.Value.RefreshToken);
+            }
+
+        }
+        [Fact]
+        public async Task Handle_ShouldReturnUnauthorized_WhenPasswordIsInvalid()
+        {
+            // Arrange
+            var context = TestFactory.CreateContext();
+            var user = TestFactory.CreateTestUser("123", "123", out var hasher);
+            var mockJwtService = TestFactory.CreateJwtServiceMock();
+            context.Users.Add(user); context.SaveChanges();
+
+            var handler = new LoginUserCommandHandler(context, hasher, mockJwtService.Object);
+            var command = new LoginUserCommand(new LoginUserDto { Username = "123", Password = "wrongpass" });
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.IsType<UnauthorizedHttpResult>(result);
+        }
+
+    }
+}
